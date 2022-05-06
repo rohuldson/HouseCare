82ee6427f1600f06529dddccbc360fc9d2b9a6c3
Ronald Silva <ronald.creative@gmail.com>
2022-05-06 20:39:50 -0300

    add Js and menu expanded

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-let scrollY = document.querySelector(".scroll");
+const nav = document.querySelector("#navigation");
+
+function onScroll() {
+  if (scrollY > 0) {
+    nav.classList.add("scroll");
+  } else {
+    nav.classList.remove("scroll");
+  }
+}
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
